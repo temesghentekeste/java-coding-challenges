67c9559d5dc23689ff2b221c2847ecbd3dd3632f
temesghen tekeste <mail.temesghen.tekeste@gmail.com>
2020-06-12 18:01:56 +0300

    Added and solved duplicate string challenge using maps and arrays

---
+++ 01-duplicate-string/DuplicateString.java
@@ -0,0 +1,33 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class DuplicateWords {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String sentence = "He is a very brave man even more brave than he thinks he is.";
+		duplicateWord(sentence);
+	}
+	
+	public static void duplicateWord(String sentence) {
+		String[] arrSentence = sentence.split(" ");
+		Map<String, Integer> wordCountMap = new HashMap<>();
+		
+		for (String word : arrSentence) {
+			word = word.toLowerCase();
+			word = word.replaceAll("[-+^]*", ""); 
+			word = word.replace(",", "");
+			word = word.replace(".", "");
+			if(wordCountMap.containsKey(word)) {
+				wordCountMap.put(word, wordCountMap.get(word) + 1);
+			}else {
+				wordCountMap.put(word, 1);
+			}
+		}
+		
+		wordCountMap.forEach((k,v) -> System.out.println(k + " " + v ));
+		
+	}
+
+}
+
