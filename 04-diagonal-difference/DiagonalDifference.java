193f2894a8b83c7408f988ee057bf56936417c07
temesghen tekeste <mail.temesghen.tekeste@gmail.com>
2020-06-19 13:14:53 +0300

    Added .class file in .gitignore file

--- 04-diagonal-difference/DiagonalDifference.java
+++ 04-diagonal-difference/DiagonalDifference.java
@@ -2,7 +2,7 @@ public class DiagonalDifference {
   public static void main(String[] args) {
     System.out.println(diagonalDifference(new int[][]{{11,2,4}, 
                                                       {4, 5, 6}, 
-                                                      {10, 8, 12}}));
+                                                      {10, 10, 12}}));
   }
 
   public static int diagonalDifference(int[][] array){
